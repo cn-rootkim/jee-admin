3634ef24ae26f5477f6cc12caa75794cd2b7f692
cn-rootkim <614890664@qq.com>
2024-05-27 09:03:59 +0800

    去除了window.location.href，如果部署的项目有父路径，window.location.href跳转会把父路径忽略掉

--- src/utils/http/axios.js
+++ src/utils/http/axios.js
@@ -1,10 +1,12 @@
 import axios from "axios";
-import { ElMessage,ElLoading } from "element-plus";
+import {ElMessage, ElLoading} from "element-plus";
+import router from "../../router/index.js";
 //1.创建axios实例
 const instance = axios.create({
-  // baseURL: "http://localhost:9000/base-service",
-    baseURL: "http://localhost:9001",
-  timeout: 50000,
+    // baseURL: "http://localhost:9000/base-service",
+    //   baseURL: "http://localhost:9001",
+    baseURL: "https://rootkim.net/rootkim-blog-api",
+    timeout: 50000,
 });
 
 let loadingInstance = null;
@@ -12,39 +14,39 @@ let loadingInstance = null;
 //2.请求拦截器
 instance.interceptors.request.use(
     (config) => {
-        loadingInstance = ElLoading.service({ fullscreen: true,background: 'rgba(0, 0, 0, 0.7)', })
-    if (config.url === "/sysUser/loginByUsernameAndPassword") {
+        loadingInstance = ElLoading.service({fullscreen: true, background: 'rgba(0, 0, 0, 0.7)',})
+        if (config.url === "/sysUser/loginByUsernameAndPassword") {
+            return config;
+        }
+        let token = localStorage.getItem("token");
+        if (token) {
+            config.headers["token"] = token;
+        }
         return config;
+    },
+    (err) => {
+        return Promise.reject(err);
     }
-    let token = localStorage.getItem("token");
-    if (token) {
-      config.headers["token"] = token;
-    }
-    return config;
-  },
-  (err) => {
-    return Promise.reject(err);
-  }
 );
 //3.响应拦截器
 instance.interceptors.response.use(
-  (response) => {
-      if(loadingInstance!=null) loadingInstance.close();
-    if (response.data.code === 200) {
-        return response.data;
-    } else if (response.data.code === 401 || response.data.code === 402) {
-        ElMessage.error(response.data.message);
-        window.location.href = "/login";
-        return Promise.reject(response.data.message);
-    } else {
-      ElMessage.error(response.data.message);
-      return Promise.reject(response.data.message);
+    (response) => {
+        if (loadingInstance != null) loadingInstance.close();
+        if (response.data.code === 200) {
+            return response.data;
+        } else if (response.data.code === 401 || response.data.code === 402) {
+            ElMessage.error(response.data.message);
+            router.push({path: "/login"});
+            return Promise.reject(response.data.message);
+        } else {
+            ElMessage.error(response.data.message);
+            return Promise.reject(response.data.message);
+        }
+    },
+    (err) => {
+        ElMessage.error("请求失败");
+        return Promise.reject(err);
     }
-  },
-  (err) => {
-    ElMessage.error("请求失败");
-    return Promise.reject(err);
-  }
 );
 //4.导出axios实例
 export default instance;
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,65 +1,68 @@
 <template>
-    <div>
-        <el-container style="width: 30%; margin-left: auto; margin-right: auto; margin-top: 10%;">
-            <el-header>
-                <div style="width: 400px;">
-                    <p style="float: left;">
-                        <el-icon color="#409EFF" :size="50">
-                            <ElementPlus />
-                        </el-icon>
-                    </p>
-                    <p style="float: left;font-size: 25px;font-weight: bold;">
-                        {{this.$systemName}}
-                    </p>
-                </div>
-            </el-header>
-            <el-main>
-                <el-card class="login_card">
-                    <el-form :model="form" :rules="rules" ref="ruleFormRef" label-width="80px">
-                        <el-form-item label="账号：" prop="username">
-                            <el-input v-model="form.username" placeholder="请输入账号"></el-input>
-                        </el-form-item>
-                        <el-form-item label="密码：" prop="password">
-                            <el-input type="password" v-model="form.password" placeholder="请输入密码" @keyup.enter="onSubmit()"></el-input>
-                        </el-form-item>
-                        <el-form-item>
-                            <el-button type="primary" @click="onSubmit()">登录</el-button>
-                            <el-button @click="resetForm()">重置</el-button>
-                        </el-form-item>
-                    </el-form>
-                </el-card>
-            </el-main>
-        </el-container>
-    </div>
+  <div>
+    <el-container style="width: 30%; margin-left: auto; margin-right: auto; margin-top: 10%;">
+      <el-header>
+        <div style="width: 400px;">
+          <p style="float: left;">
+            <el-icon color="#409EFF" :size="50">
+              <ElementPlus/>
+            </el-icon>
+          </p>
+          <p style="float: left;font-size: 25px;font-weight: bold;">
+            {{ this.$systemName }}
+          </p>
+        </div>
+      </el-header>
+      <el-main>
+        <el-card class="login_card">
+          <el-form :model="form" :rules="rules" ref="ruleFormRef" label-width="80px">
+            <el-form-item label="账号：" prop="username">
+              <el-input v-model="form.username" placeholder="请输入账号"></el-input>
+            </el-form-item>
+            <el-form-item label="密码：" prop="password">
+              <el-input type="password" v-model="form.password" placeholder="请输入密码"
+                        @keyup.enter="onSubmit()"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="onSubmit()">登录</el-button>
+              <el-button @click="resetForm()">重置</el-button>
+            </el-form-item>
+          </el-form>
+        </el-card>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 <script setup>
 import sysUserApi from "../api/sys/sysUser.js";
-import { reactive, ref } from "vue";
-import { ElMessage } from "element-plus";
+import {reactive, ref} from "vue";
+import {ElMessage} from "element-plus";
 import router from "../router/index";
+
 const form = reactive({
-    username: "",
-    password: "",
+  username: "",
+  password: "",
 });
 const ruleFormRef = ref();
 const rules = reactive({
-    username: [{ required: true, message: "请输入账号", trigger: "blur" }],
-    password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+  username: [{required: true, message: "请输入账号", trigger: "blur"}],
+  password: [{required: true, message: "请输入密码", trigger: "blur"}],
 });
 const onSubmit = () => {
-    if(!ruleFormRef) return;
-    ruleFormRef.value.validate(async (valid) => {
-        if (valid) {
-            const res = await sysUserApi.loginByUsernameAndPassword(form);
-            ElMessage.success("登录成功");
-            localStorage.setItem("token", res.data.token);
-            window.location.href = "/home";
-        } else {
-            return false;
-        }
-    });
+  if (!ruleFormRef) return;
+  ruleFormRef.value.validate(async (valid) => {
+    if (valid) {
+      const res = await sysUserApi.loginByUsernameAndPassword(form);
+      ElMessage.success("登录成功");
+      localStorage.setItem("token", res.data.token);
+      await router.push({path: "/home"});
+    } else {
+      return false;
+    }
+  });
 };
 const resetForm = () => {
-    if (!ruleFormRef) return; ruleFormRef.value.resetFields();
+  if (!ruleFormRef) return;
+  ruleFormRef.value.resetFields();
 };
 </script>
